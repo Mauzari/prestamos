3490be047e94888cac8ddc9d4baca0008b32da84
Mauricio Riffo <mauri.riffo@gmail.com>
2022-02-26 12:39:06 -0300

    Prestamos con react
    Subida del proyecto a github

---
+++ src/componentes/Header.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+/* function Header({titulo}) {
+
+    return(
+        <Fragment>
+            <h1>{ titulo }</h1>
+        </Fragment>
+    )
+} */
+
+// Otra forma de crear el componente
+const Header = ({titulo}) => (   
+            <h1>{ titulo }</h1>   
+     );
+
+ 
+export default Header;
+
